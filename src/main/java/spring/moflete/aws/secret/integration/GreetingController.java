26bb3fb5513ded65651984b4b8a3d9e46731ae03
Camilo Contreras <contreras.camilo32@gmail.com>
2023-11-17 00:06:09 -0300

    Aws secret retrieve value implementation

--- src/main/java/spring/moflete/aws/secret/integration/GreetingController.java
+++ src/main/java/spring/moflete/aws/secret/integration/GreetingController.java
@@ -1,13 +1,22 @@
 package spring.moflete.aws.secret.integration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class GreetingController {
 
+    @Value("${my-test-value}")
+    private String awsKey;
+
     @GetMapping("/greeting")
     public String greet() {
-        return "Hello! Welcome to the Java Spring Boot REST API.";
+        String rawThing = awsKey;
+        Optional<String> meeting = Optional.of(awsKey);
+
+        return "Hello! Welcome to the Java Spring Boot REST API - ";
     }
 }
